5939d45e1d45f78efa0a642c753fd4d1db86a7d3
Hexbob6 <sam.walker.email@gmail.com>
2017-04-13 22:30:50 +0100

    Second Revision of code
    Testing to see if nicescroll.js will be updated or whether it will add a whole new file

--- scripts/nicescroll.js
+++ scripts/nicescroll.js
@@ -6,9 +6,9 @@ $(document).ready(
             
             scrollspeed : "100",
             mousescrollstep : "60",
-            cursorcolor : "#f1f1f1",
+            cursorcolor : "#515151",
             cursorwidth : 10,
-            cursorborder : "none",
+            cursorborder : "1px solid rgba(255,255,255,0.6)",
             cursoropacitymax : 0.5,
             zindex : 9999,
 //            touchbehavior : true,
@@ -16,6 +16,20 @@ $(document).ready(
             
         });
         
+        $(".reviews-scroller").niceScroll({
+            
+            scrollspeed : "100",
+            mousescrollstep : "60",
+            cursorcolor : "#f1f1f1",
+            cursorwidth : 15,
+            cursorborderradius : "8px",
+            cursorborder : "0px",
+            autohidemode : false,
+//            touchbehavior : true,
+//            grabcursorenabled : false,
+            
+        });
+        
     }
     
-);
+)
