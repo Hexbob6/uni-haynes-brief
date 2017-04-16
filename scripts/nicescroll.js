dc2c409bb5df6662622b878130161c13c60ada9e
Hexbob6 <sam.walker.email@gmail.com>
2017-04-16 04:09:23 +0100

    Updating Nicescroll.js
    Added a condition that removes nicescroll on <640px screens. Testing whether it will allow the overlay elements to be scrolled on mobile.

--- scripts/nicescroll.js
+++ scripts/nicescroll.js
@@ -2,49 +2,53 @@ $(document).ready(
     
     function() {
         
-        $("html").niceScroll({
-            
-            smoothscroll : true,
-            scrollspeed : "100",
-            mousescrollstep : "60",
-            cursorcolor : "#515151",
-            cursorwidth : 10,
-            cursorborder : "1px solid rgba(255,255,255,0.6)",
-            cursoropacitymax : 0.5,
-            zindex : 9999,
-//            touchbehavior : true,
-//            grabcursorenabled : false,
-            
-        });
+        if ($(window).width() > 640) {
         
-        $(".reviews-scroller").niceScroll({
+            $("html").niceScroll({
+
+                smoothscroll : true,
+                scrollspeed : "100",
+                mousescrollstep : "60",
+                cursorcolor : "#515151",
+                cursorwidth : 10,
+                cursorborder : "1px solid rgba(255,255,255,0.6)",
+                cursoropacitymax : 0.5,
+                zindex : 9999,
+    //            touchbehavior : true,
+    //            grabcursorenabled : false,
+
+            });
+
+            $(".reviews-scroller").niceScroll({
+
+                smoothscroll : true,
+                scrollspeed : "100",
+                mousescrollstep : "60",
+                cursorcolor : "#f1f1f1",
+                cursorwidth : 15,
+                cursorborderradius : "8px",
+                cursorborder : "0px",
+                autohidemode : false,
+    //            touchbehavior : true,
+    //            grabcursorenabled : false,
+
+            });
+
+            $(".features-text").niceScroll({
+
+                smoothscroll : true,
+                scrollspeed : "100",
+                mousescrollstep : "60",
+                cursorcolor : "#f1f1f1",
+                cursorwidth : 12,
+                cursorborder : "0px",
+                cursoropacitymax : 0.5,
+                touchbehavior : true,
+                grabcursorenabled : false,
+
+            });
             
-            smoothscroll : true,
-            scrollspeed : "100",
-            mousescrollstep : "60",
-            cursorcolor : "#f1f1f1",
-            cursorwidth : 15,
-            cursorborderradius : "8px",
-            cursorborder : "0px",
-            autohidemode : false,
-//            touchbehavior : true,
-//            grabcursorenabled : false,
-            
-        });
-        
-        $(".features-text").niceScroll({
-            
-            smoothscroll : true,
-            scrollspeed : "100",
-            mousescrollstep : "60",
-            cursorcolor : "#f1f1f1",
-            cursorwidth : 12,
-            cursorborder : "0px",
-            cursoropacitymax : 0.5,
-            touchbehavior : true,
-            grabcursorenabled : false,
-            
-        });
+        }
         
     }
     
